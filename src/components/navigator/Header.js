69408246b296ae3f4e29b8de80d7022c5ae72990
drigues <drg.thiago@gmail.com>
2018-03-15 11:18:33 +0000

    styling header

--- src/components/navigator/Header.js
+++ src/components/navigator/Header.js
@@ -4,11 +4,32 @@ import PropTypes from 'prop-types';
 
 const Header = ({}) => {
   return (
-    <header className='header'>
-      <h2>Header</h2>
-    </header>
+    <nav className="header navbar navbar-expand-lg navbar-light bg-light">
+      <div className="logo navbar-brand">
+        <h1>Fredfy</h1>
+        <h2>Learn_</h2>
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExampleDefault"
+        aria-controls="navbarsExampleDefault"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">Sign Up</li>
+          <li>Sign In</li>
+          <li>Profile</li>
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
 
 Header.propTypes = {
   // : PropTypes.
