f3e72f914e74ccb8ea75789bca08110be45b780b
leoancap <leoancap@gmail.com>
2018-04-07 15:34:19 +0100

    fixing challenge routes and challenge folder

---
+++ src/components/challenge/ChallengePage.js
@@ -0,0 +1,53 @@
+// ********** REACT ********** //
+import React from "react";
+
+// ********** api ************ //
+import db from "../../fixtures/challenges";
+
+// ******** REDUX **********//
+import { connect } from "react-redux";
+
+const ListChallenges = props => {
+  const challenges = Object.keys(props.challenges);
+  const listItems = challenges.map(challenge => (
+    <Link
+      key={challenge.toString()}
+      to={`/challenges-you/${challenge}`}
+      onClick={() => {}}
+    >
+      <li key={challenge.toString()}>{challenge}</li>
+    </Link>
+  ));
+  return <ul>{listItems}</ul>;
+};
+
+class ChallengePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      challenges: {}
+    };
+  }
+
+  handleChosenChallenges = () => {};
+
+  componentDidMount = () => {
+    console.log(this.props.history);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>ChallengePage</h2>
+        <div />
+      </div>
+    );
+  }
+}
+
+ChallengePage.propTypes = {
+  // : PropTypes.
+};
+
+export default ChallengePage;
