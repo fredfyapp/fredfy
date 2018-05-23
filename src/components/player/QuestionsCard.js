a56a4cb7b7a72bef8cc10d43ccc5b8e89dd7cd5f
Marcel Cruz <marcelcruz@live.com>
2018-05-23 21:02:50 +0100

    progressbar component removed

--- src/components/player/QuestionsCard.js
+++ src/components/player/QuestionsCard.js
@@ -8,12 +8,10 @@ import ProgressBar from './ProgressBar';
 import Question from './Question';
 
 class QuestionsCard extends React.Component {
-
   render() {
     const subjectName = this.props.match.params.subject;
     return (
       <div>
-        <ProgressBar />
         <Question
           questions={this.props.shuffledQuestions}
           subjectName={subjectName}
@@ -22,7 +20,6 @@ class QuestionsCard extends React.Component {
       </div>
     );
   }
-
 }
 
 QuestionsCard.propTypes = {
